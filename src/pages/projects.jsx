3837930e0febe4eca14bb044c3acbbd00ca3c6a7
Ethan Freeman <ethanfree23@gmail.com>
2022-09-29 11:20:11 -0500

    projects page

--- src/pages/projects.jsx
+++ src/pages/projects.jsx
@@ -11,40 +11,40 @@ import logoPlanetaria from '@/images/logos/planetaria.svg'
 
 const projects = [
   {
-    name: 'Planetaria',
+    name: 'FAC',
     description:
-      'Creating technology to empower civilians to explore space on their own terms.',
-    link: { href: 'http://planetaria.tech', label: 'planetaria.tech' },
+      'An app that allows companies and individuals to manage their art rotations.',
+    link: { href: 'http://www.freeemanartcompany.com', label: 'freemanartcompany.com' },
     logo: logoPlanetaria,
   },
   {
-    name: 'Animaginary',
+    name: 'HubOEM',
     description:
-      'High performance web animation library, hand-written in optimized WASM.',
+      'A headless ecommerce platform built for consumer packaged goods manufacturers to access documents, order parts, and request services, across manufacturers, all in one place.',
     link: { href: '#', label: 'github.com' },
     logo: logoAnimaginary,
   },
   {
-    name: 'HelioStream',
+    name: 'Infosys',
     description:
-      'Real-time video streaming library, optimized for interstellar transmission.',
+      'A software development company based in India',
     link: { href: '#', label: 'github.com' },
     logo: logoHelioStream,
   },
-  {
-    name: 'cosmOS',
-    description:
-      'The operating system that powers our Planetaria space shuttles.',
-    link: { href: '#', label: 'github.com' },
-    logo: logoCosmos,
-  },
-  {
-    name: 'OpenShuttle',
-    description:
-      'The schematics for the first rocket I designed that successfully made it to orbit.',
-    link: { href: '#', label: 'github.com' },
-    logo: logoOpenShuttle,
-  },
+  // {
+  //   name: 'cosmOS',
+  //   description:
+  //     'The operating system that powers our Planetaria space shuttles.',
+  //   link: { href: '#', label: 'github.com' },
+  //   logo: logoCosmos,
+  // },
+  // {
+  //   name: 'OpenShuttle',
+  //   description:
+  //     'The schematics for the first rocket I designed that successfully made it to orbit.',
+  //   link: { href: '#', label: 'github.com' },
+  //   logo: logoOpenShuttle,
+  // },
 ]
 
 function LinkIcon(props) {
